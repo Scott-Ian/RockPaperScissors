5b725bceee38403372ff1b38daf4aecbcfc9da52
Ian Scott <chesnekov@gmail.com>
2020-07-15 14:09:09 -0700

    Adds successfully passing test for draw conditions
    co-authored-by: Evgeniya Chernaya <evgenya.chernaya@gmail.com>

--- RockPaperScissors.Tests/ModelTests/GameTests.cs
+++ RockPaperScissors.Tests/ModelTests/GameTests.cs
@@ -14,12 +14,22 @@ namespace RockPaperScissors.TestTools
     }
 
     [TestMethod]
-    public void RPSComparison_DeterminesWinner_Player()
+    public void RPSComparison_DeterminesWinner_true()
     {
       Player rockPlayer = new Player("R");
       Player scissorsPlayer = new Player("S");
       Player.RPSComparison(rockPlayer, scissorsPlayer);
       Assert.AreEqual(true, rockPlayer.IsWin);
+    }
+
+    [TestMethod]
+    public void RPSComparison_DeterminesNoWinner_false()
+    {
+      Player rockPlayer = new Player("R");
+      Player scissorsPlayer = new Player("R");
+      Player.RPSComparison(rockPlayer, scissorsPlayer);
+      Assert.AreEqual(false, rockPlayer.IsWin);
+      Assert.AreEqual(false, scissorsPlayer.IsWin);
     }
   }
 }
